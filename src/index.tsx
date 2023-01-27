02002d3bc25f8a6240207e6c6de534926f293fb3
lz.piotr <piotr@layerzerolabs.org>
2023-01-27 03:19:05 +0100

    rainbowkit integration example

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import {
   Bridge,
   bootstrap,
@@ -12,11 +11,13 @@ import {
 
 import { wagmi } from "./config/wagmi";
 import { testnet } from "./config/testnet";
+import { ConnectButton, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 import "./styles/reset.css";
 import "./styles/fonts.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import "react-toastify/dist/ReactToastify.css";
+import { WagmiConfig } from "wagmi";
 
 // boostrap the bridge
 bootstrap(testnet, {
@@ -58,13 +59,20 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// don't use strict mode with rainbowkit
+// see: https://github.com/rainbow-me/rainbowkit/issues/836
+root.render(
+  <>
+    {/* use your own rainbow kit provider */}
+    <WagmiConfig client={wagmi.wagmiClient as any}>
+      <RainbowKitProvider chains={wagmi.wagmiChains}>
+        {/* you use connect button from rainbowkit */}
+        <ConnectButton />
+
+        {/* bridge will use existing integration */}
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </>
+);
