45bf31cb438dc807f5484e3f953679f918fdc607
Weston Nelson <dogooddowell@protonmail.com>
2023-07-25 01:24:09 +0000

    Update Tokens

--- src/config/mainnet.ts
+++ src/config/mainnet.ts
@@ -2,16 +2,11 @@ import {ChainId} from '@layerzerolabs/lz-sdk';
 import {Token} from '@layerzerolabs/x-trader-joe-bridge';
 
 const tokens = [
-  new Token(ChainId.BSC, '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07', 18, 'JOE'),
-  new Token(ChainId.ARBITRUM, '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07', 18, 'JOE'),
-  new Token(ChainId.AVALANCHE, '0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd', 18, 'JOE'),
+  new Token(ChainId.BSC, '0xed1fa3155ebB4cE35DA3e2f8B122717B823BDFa1', 18, 'ONFTE'),
+  new Token(ChainId.ARBITRUM, '0x59d5cA4D8f4428C5AAc66f7D91D115F408b847ce', 18, 'ONFTE'),
+  new Token(ChainId.AVALANCHE, '0x2B041419A471b9aDB996687B3D27Fbbbf6340A4f', 18, 'ONFTE'),
+  new Token(ChainId.OPTIMISM, '0x3134F3ff6C0DAf74B32fc683B4098F9de0086589', 18, 'ONFTE'),
+  new Token(ChainId.POLYGON, '0xc6a9d92d5D39D7091a262F964285FB75aaBB2a48', 18, 'ONFTE'),
 ];
 
-const proxy = [
-  {
-    chainId: ChainId.AVALANCHE,
-    address: '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07',
-  },
-];
-
-export const mainnet = {tokens, proxy};
+export const mainnet = {tokens};
